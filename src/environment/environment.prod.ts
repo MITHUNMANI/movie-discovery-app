e466edf28dcb3442bba9dc7ee97bcf726a265622
Mithun Mani <mmani@edmentum.com>
2025-09-08 02:22:43 +0530

    hotfix: deployment build code

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,7 +2,8 @@ import { Environment } from './environment.model';
 import { environment as base } from './environment';
 
 export const environment: Environment = {
-  ...base,
+  production: true,
   tmdbApiKey: 'c78a827744d726be47cb2612a862f82c',
-  production: true
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
